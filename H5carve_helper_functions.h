f89403c9c46c1aea1f8b43a476aa7cdda8c379c1
Raffay Atiq <raffayatiq2341@gmail.com>
2024-06-17 19:19:00 +0000

    Added minimal netCDF4 support, excluding attributes and fallback machinery.

--- H5carve_helper_functions.h
+++ H5carve_helper_functions.h
@@ -2,6 +2,7 @@
 #define H5CARVE_HELPER_FUNCTIONS_H
 
 herr_t copy_attributes(hid_t loc_id, const char *name, const H5L_info_t *linfo, void *opdata);
+herr_t delete_attributes(hid_t loc_id, const char *name, const H5L_info_t *linfo, void *opdata);
 herr_t shallow_copy_object(hid_t loc_id, const char *name, const H5L_info_t *linfo, void *opdata);
 void count_objects_in_group(hid_t loc_id, const char *name, const H5L_info_t *linfo, void *opdata);
 char *get_carved_filename(const char *filename);
